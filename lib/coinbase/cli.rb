5e7a5b66f7cc105e57120bfc599d44bd6b105e51
connor worthen <connorw220@protonmail.com>
2019-12-05 21:48:52 +0000

    first commit

---
+++ lib/coinbase/cli.rb
@@ -0,0 +1,8 @@
+require "coinbase/cli/version"
+
+module Coinbase
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/coinbase/cli/version.rb
@@ -0,0 +1,5 @@
+module Coinbase
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
