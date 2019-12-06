b343f785a9c441b42e4802ecd085123fb9a572e6
connor worthen <connorw220@protonmail.com>
2019-12-06 22:13:46 +0000

    modified config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,5 @@ require 'open-uri'
 require 'nokogiri'
 
 require_relative '../lib/crypto_prices/crypto.rb'
-require_relative '../lib/crypto_prices/prices.rb'
+require_relative '../lib/crypto_prices/cli.rb'
 require_relative '../lib/crypto_prices/scraper.rb'
