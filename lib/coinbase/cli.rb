ba17aeaa4fcbfcbbf9919cd4428dcc21fcfbddcb
connor worthen <connorw220@protonmail.com>
2019-12-06 05:59:51 +0000

    modified lib/cli.rb

--- lib/coinbase/cli.rb
+++ lib/coinbase/cli.rb
@@ -7,14 +7,6 @@ class CLI
     start
   end
 
-  def start
-    puts ""
-    create_coins
-    crypto_prices
-    extra_detail
-    end_cli
-  end
-
   def create_coins
     if Coin.all.empty?
       coins_array = Scraper.new.scrape_index_page
@@ -38,7 +30,6 @@ class CLI
     description = Scraper.scrape_description(single.url)
     Coin.update_description(single, description)
     puts "#{single.name}" + " - (#{single.short_code.upcase})"
-    puts ""
     puts " Price:" + " #{single.price}"
     puts " Market Cap" + " #{single.market_cap}"
     puts " Description:" + " #{single.description}"
@@ -46,40 +37,31 @@ class CLI
   end
 
   def more_detail
-    puts ""
     puts "To view more specific information on a particular coin, enter the coins shortcode or enter ALL, to see the comprehensive list of all available cryptocurrencies! If you wish to exit the program, enter BYE."
     input = gets.strip.upcase
-    puts ""
-    puts ""
     if Coin.find_by_name(input)
       coin_detail(input)
     elsif input == "BYE"
-      puts ""
       puts "Thank you for stopping by and may the odds be in your favor!"
       exit
     elsif input == "All"
       start
     else
-      puts ""
       puts "I cannot understand your input, please try again using the coins shortcode."
       start
     end
   end
 
   def end_cli
-    puts ""
       puts "To access more information on a particular coin enter Y, if not enter N."
       input = gets.strip.downcase
       if input == "y"
         start
       elsif input == "n"
-        puts ""
         puts "Thank you for stopping by and may the odds be in your favor!"
         exit
       else
-        puts ""
         puts "I cannot understand your input, please try again using the coins shortcode."
-        puts ""
         start
       end
   end
