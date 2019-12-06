b4f712641d85317deb9fd6823d72c6b9ced89b00
connor worthen <connorw220@protonmail.com>
2019-12-06 06:21:54 +0000

    modified lib/prices.rb

--- lib/coinbase/prices.rb
+++ lib/coinbase/prices.rb
@@ -1,7 +1,7 @@
-require "pry"
+require 'pry'
 require_relative './crypto.rb'
 require_relative './scraper.rb'
-class Cli::CLI
+class Prices::CLI
 
   def intro
     puts "Greetings fellow cyborgs and welcome to my very own Coinbase CLI! In this program, you will be able to view different tradeable cryptocurrencies from coinbase.com."
