ac847e2a011a05782264ef40aad707ace5c321a0
connor worthen <connorw220@protonmail.com>
2019-12-07 20:59:35 +0000

    modified lib/scraper.rb

--- lib/crypto_prices/scraper.rb
+++ lib/crypto_prices/scraper.rb
@@ -7,12 +7,12 @@ class Scraper
     price_page = Nokogiri::HTML(open("https://www.coinbase.com/price"))
     coins = []
     price_page.css("tr")[1..13].each do |coin|
-      coin_name = coin.css("span.AssetRow__BrandedTitle-kQVjGE.yfMRP").text
-      coin_price = coin.css("div.Flex-NZkZp.gICBdl").text
-      coin_market_cap = coin.css("td.AssetRow__Td-gjXXut.AssetRow__Cap-ciIRMg.dJJSHO").text
+      coin_name = coin.css("span > h4").text.strip
+      coin_price = coin.css("div > h4").text.strip
+      coin_market_cap = coin.css("span > h4").text.strip
       coin_url = "https://www.coinbase.com" + coin.at_css("td a[href]")['href']
-      coin_shortcode = coin.css("div > h4").text
-      coin_percent_change = coin.css("tr").text
+      coin_shortcode = coin.css("div > h4").text.strip
+      coin_percent_change = coin.css("tr > h4").text.strip
       coins << {name: coin_name, short_code: coin_shortcode, url: coin_url, price: coin_price, market_cap: coin_market_cap}
     end
     coins
