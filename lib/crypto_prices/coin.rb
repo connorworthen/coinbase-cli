26bcb6de3c5dabd12f066563bd684562a8fa6905
connor worthen <connorw220@protonmail.com>
2019-12-09 04:15:30 +0000

    modified lib/coin.rb

--- lib/crypto_prices/coin.rb
+++ lib/crypto_prices/coin.rb
@@ -6,25 +6,40 @@ class Coin
   
   @@all = []
   
-  def initialize(price, name, description)
-    @price = price
+  def initialize(name)
     @name = name
-    @description = description
     @@all << self
   end
   
   def price=(price)
-  
+    @price = price
   end
   
   def name=(name)
-    
+    @name = name
   end
   
   def description=(description)
+    @description = description
     
   end
   
 end
- 
+
+  Bitcoin = Coin.new("Bitcoin")
+  Bitcoin.price=(Scraper.scrape_price_page[0])
+  Bitcoin.description=(Scraper.scrape_coin_page[0])
+  binding.pry
+  # Ethereum = Coin.new(Scraper.scrape_coin_page[1])
+  
+  
+  # Ripple = Coin.new(Scraper.scrape_coin_page[2])
+  
+  
+  # Bitcoin Cash = Coin.new(Scraper.scrape_coin_page[3])
+  
+  
+  # Litecoin = Coin.new(Scraper.scrape_coin_page[4])
+  
+  
     
