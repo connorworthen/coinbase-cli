ed322cc8e6c1a4c4240261c31455476ce43d2425
connor worthen <connorw220@protonmail.com>
2019-12-09 01:58:57 +0000

    modified lib/scraper.rb

--- lib/crypto_prices/scraper.rb
+++ lib/crypto_prices/scraper.rb
@@ -26,13 +26,25 @@ class Scraper
   def self.scrape_coin_page
     url = open("https://www.coinbase.com/price/bitcoin")
     url_2 = open("https://www.coinbase.com/price/ethereum")
+    url_3 = open("https://www.coinbase.com/price/ripple")
+    url_4 = open("https://www.coinbase.com/price/bitcoin-cash")
+    url_5 = open("https://www.coinbase.com/price/litecoin")
     doc = Nokogiri::HTML(url)
     doc_2 = Nokogiri::HTML(url_2)
+    doc_3 = Nokogiri::HTML(url_3)
+    doc_4 = Nokogiri::HTML(url_4)
+    doc_5 = Nokogiri::HTML(url_5)
     coins = []
     coin = doc.css(".AssetInfo__DescriptionText-sc-4v99na-3.hnkcSy").text
     coin_2 = doc_2.css(".AssetInfo__DescriptionText-sc-4v99na-3.hnkcSy").text
+    coin_3 = doc_3.css(".AssetInfo__DescriptionText-sc-4v99na-3.hnkcSy").text
+    coin_4 = doc_4.css(".AssetInfo__DescriptionText-sc-4v99na-3.hnkcSy").text
+    coin_5 = doc_5.css(".AssetInfo__DescriptionText-sc-4v99na-3.hnkcSy").text
     coins << coin
     coins << coin_2
+    coins << coin_3
+    coins << coin_4
+    coins << coin_5
   coins
   end
   
