675c545d1675970e9c89f60a93f28daf8bfdcd41
connor worthen <connorw220@protonmail.com>
2019-12-11 05:48:49 +0000

    modified lib/menu.rb

--- lib/crypto_prices/menu.rb
+++ lib/crypto_prices/menu.rb
@@ -17,16 +17,18 @@ class Menu
   def intro
     puts ""
     puts "In this program, you will be able to view different tradeable cryptocurrencies,"
-    puts "their current rates, descriptions, and market cap."
+    puts "their current rates, descriptions, and facts about cryptocurrencies."
     puts ""
     puts "---------------------------"
-    puts "Select a number from 1 - 4"
+    puts "Select a number from 1 - 5"
     puts "---------------------------"
     puts ""
-    puts (" 1. ") + "Enter 1, to see list of all different coins."
-    puts (" 2. ") + "Enter 2, to see the current rate and description of a listed coin."
-    puts (" 3. ") + "Return to main menu."
-    puts (" 4. ") + "Exit"
+    puts (" 1. ") + "Enter 1, to see current prices of the top 5 cryptocurrencies."
+    puts (" 2. ") + "Enter 2, to see the description of the top 5 cryptocurrencies."
+    puts (" 3. ") + "Enter 3, to see 3 facts about cryptocurrencies"
+    puts (" 4. ") + "Enter 4, to see where to buy and trade cryptocurrencies."
+    puts (" 5. ") + "Enter 5, to return to the menu."
+    puts (" 6. ") + "Enter 6, to exit the program."
     puts ""
     puts ""
   end
@@ -38,18 +40,22 @@ class Menu
      case input
        when "1"
          puts ""
-         all_bios
+         price_bios
       when "2"
          puts ""
-         price_bios
+         all_bios
       when "3"
         puts ""
-        menu
+        facts
       when "4"
-         puts "Thank  you  for  using  my very own Coinbase CLI"
+        buy
+      when "5"
+        menu
+      when "6"
+         puts "Thank  you  for  using  my very own Coinbase CLI and happy trading!"
          break
          else
-         puts "That option does not exist. Please select a number from 1 - 4 "
+         puts "That option does not exist. Enter 5, to return to the main menu or 6 to exit the program! "
      end
    end
   end
@@ -61,8 +67,8 @@ class Menu
     input = " "
     while input != '0'
       puts ""
-      puts "Choose a Devil Fruit(#1-3) to see its abilities!"
-      puts "If you would like to go back to the menu, type '0'"
+      puts "Enter the coins number to see the coins description."
+      puts "If you would like to go back to the menu, press enter, then type '5'"
       bios = Coin.all
       input = gets.strip.to_i
         if (1..Coin.all.length).include?(input)
@@ -81,8 +87,8 @@ class Menu
     input = " "
     while input != '0'
       puts ""
-      puts "Choose a Devil Fruit(#1-3) to see its abilities!"
-      puts "If you would like to go back to the menu, type '0'"
+      puts "Enter the coins number to see the coins current rates."
+      puts "If you would like to go back to the menu, press enter, then type '5'"
       bios = Coin.all
       input = gets.strip.to_i
         if (1..Coin.all.length).include?(input)
@@ -94,12 +100,18 @@ class Menu
       end
   end
   
-  def description
-    puts "test"
+  def facts
+    puts "1. Cryptocurrencies are extremly volatility which is one of the reasons why many investors choose to steer away from the Crypto-verse. If the value can swing dramatically, it means that you can potentially make large profits, but also large losses."
+    puts ""
+    puts "2. We all know that the creation of Bitcoin is credited to Satoshi Nakamoto, although we have no idea whatsoever if there’s a single person or a whole group of people behind the pseudonym. The thing is, the mysterious Satoshi Nakamoto never claimed the ownership of the original code, which means that it belongs to everyone… and no-one"
+    puts ""
+    puts "3. Remember, there’s no way back if you lose your private key(crypto wallet). Once lost, your digital funds will disappear into the giant Crypto-void. Unless hacked, which is virtually impossible considering the structure of blockchain technology, your abandoned e-wallet will be lingering out there somewhere."
+    puts ""
   end
   
-  def market_cap
-    puts "test"
+  def buy
+    puts "You can purchase different Cryptocurrencies from coinbase.com! Coinbase offers fiat onramps, or the ability to purchase coins with traditional fiat currencies. Bitcoin, Ethereum, Litecoin, and many more can all be purchased with fiat currencies on Coinbase."
+    puts ""
   end
 
 end
