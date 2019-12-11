016304901ef135af6dffe30e65267a5fa7093f3e
connor worthen <connorw220@protonmail.com>
2019-12-11 03:18:21 +0000

    modified lib/scraper.rb

--- lib/crypto_prices/coin.rb
+++ lib/crypto_prices/coin.rb
@@ -20,4 +20,12 @@ end
 
 bitcoin = Coin.new("Bitcoin(BTC)")
 bitcoin.description = (Scraper.bitcoin)
-binding.pry 
+ethereum = Coin.new("ethereum(ETH)")
+ethereum.description = (Scraper.ethereum)
+ripple = Coin.new("Ripple(XRP")
+ripple.description = (Scraper.ripple)
+bitcoin_cash = Coin.new("Bitcoin cash(BCH")
+bitcoin_cash.description = (Scraper.bitcoin_cash)
+litecoin = Coin.new("Litecoin(LTC)")
+litecoin.description = (Scraper.litecoin)
+#binding.pry 
--- lib/crypto_prices/scraper.rb
+++ lib/crypto_prices/scraper.rb
@@ -29,21 +29,39 @@ class Scraper
   end
   
   def self.litecoin
-    url_5 = open("https://www.coinbase.com/price/litecoin")
+    url = open("https://www.coinbase.com/price/litecoin")
     doc = Nokogiri::HTML(url)
     coin = doc.css(".AssetInfo__DescriptionText-sc-4v99na-3.hnkcSy").text
   end
   
-  def self.scrape_price_page
+  def self.bitcoinprice
     url = open("https://www.coinbase.com/price/bitcoin")
-    url_2 = open("https://www.coinbase.com/price/ethereum")
     doc = Nokogiri::HTML(url)
-    doc_2 = Nokogiri::HTML(url)
-    prices = []
     price = doc.css(".ChartPriceHeader__BigAmount-sc-9ry7zl-4.dKeshi").text
-    price_2 = doc_2.css(".ChartPriceHeader__BigAmount-sc-9ry7zl-4.dKeshi").text
-    prices << price
-    prices << price_2
   end
-    
+  
+  def self.ethereumprice
+    url = open("https://www.coinbase.com/price/ethereum")
+    doc = Nokogiri::HTML(url)
+    price = doc.css(".ChartPriceHeader__BigAmount-sc-9ry7zl-4.dKeshi").text
+  end
+  
+  def self.rippleprice
+    url = open("https://www.coinbase.com/price/ripple")
+    doc = Nokogiri::HTML(url)
+    price = doc.css(".ChartPriceHeader__BigAmount-sc-9ry7zl-4.dKeshi").text
+  end
+  
+  def self.bitcoincashprice
+    url = open("https://www.coinbase.com/price/bitcoin-cash")
+    doc = Nokogiri::HTML(url)
+    price = doc.css(".ChartPriceHeader__BigAmount-sc-9ry7zl-4.dKeshi").text
+  end
+  
+  def self.litecoincoinprice
+    url = open("https://www.coinbase.com/price/litecoin")
+    doc = Nokogiri::HTML(url)
+    price = doc.css(".ChartPriceHeader__BigAmount-sc-9ry7zl-4.dKeshi").text
+  end
+  
 end
